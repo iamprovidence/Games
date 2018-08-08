f625bdfd4e4ff90231c381e101c2d8c7f1e91f76
Kizlo Taras <orleon.nilkford@gmail.com>
2018-08-08 09:45:11 +0300

    Add files via upload

---
+++ roll_a_ball/script/moving.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class moving : MonoBehaviour 
+{
+	public float speed;
+	private Rigidbody rb;
+	public Text text, wintext;
+	private int count = 0;
+
+	void Start()
+	{
+		rb = GetComponent<Rigidbody>();
+		wintext.gameObject.SetActive (false);
+		setCount ();
+	}
+
+	void FixedUpdate()
+	{
+		float moveHorizontal = Input.GetAxis("Horizontal");
+		float moveVertical = Input.GetAxis("Vertical");
+
+		Vector3 movement = new Vector3(moveHorizontal, 0.0f, moveVertical);
+
+		rb.AddForce(movement * speed);
+		if (count == 7) 
+		{
+			wintext.gameObject.SetActive (true);
+		}
+	}
+
+	void OnTriggerEnter (Collider other) 
+	{
+		if (other.tag == "point") 
+		{
+			Destroy (other.gameObject);
+			++count;
+			setCount ();
+		}
+	} 
+
+	void setCount()
+	{
+		text.text = "Counter = " + count.ToString();
+	}
+}
